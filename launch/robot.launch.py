860281b100a328dc24d607cf010968cbd930771e
Veloce spex <sele4416@gmail.com>
2025-01-08 19:13:15 +0300

    working ros2_control

--- launch/robot.launch.py
+++ launch/robot.launch.py
@@ -7,48 +7,16 @@ from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument
 from launch.substitutions import LaunchConfiguration
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch_ros.actions import Node
-
+from launch.actions import IncludeLaunchDescription, TimerAction
+from launch.actions import RegisterEventHandler
+from launch.event_handlers import OnProcessStart
+from launch.substitutions import Command
 
 def generate_launch_description():
     # Define the robot's name and package name
-    robot_name = "differential_drive_robot"
     robot_pkg_name = "differential_drive_robot"
 
-    # Define a launch argument for the world file, defaulting to "empty.sdf"
-    world_arg = DeclareLaunchArgument(
-        'world',
-        default_value='empty.sdf',
-        description='Specify the world file for Gazebo (e.g., empty.sdf)'
-    )
-
-    # Define launch arguments for initial pose
-    x_arg = DeclareLaunchArgument(
-        'x', default_value='0.0', description='Initial X position')
-
-    y_arg = DeclareLaunchArgument(
-        'y', default_value='0.0', description='Initial Y position')
-
-    z_arg = DeclareLaunchArgument(
-        'z', default_value='0.5', description='Initial Z position')
-
-    roll_arg = DeclareLaunchArgument(
-        'R', default_value='0.0', description='Initial Roll')
-
-    pitch_arg = DeclareLaunchArgument(
-        'P', default_value='0.0', description='Initial Pitch')
-
-    yaw_arg = DeclareLaunchArgument(
-        'Y', default_value='0.0', description='Initial Yaw')
-
-    # Retrieve launch configurations
-    world_file = LaunchConfiguration('world')
-    x = LaunchConfiguration('x')
-    y = LaunchConfiguration('y')
-    z = LaunchConfiguration('z')
-    roll = LaunchConfiguration('R')
-    pitch = LaunchConfiguration('P')
-    yaw = LaunchConfiguration('Y')
-
+    
     # Set paths to Xacro model and configuration files
     robot_model_path = os.path.join(
         get_package_share_directory(robot_pkg_name),
@@ -56,74 +24,35 @@ def generate_launch_description():
         'robot.xacro'
     )
 
-    gz_bridge_params_path = os.path.join(
-        get_package_share_directory(robot_pkg_name),
-        'config',
-        'gz_bridge.yaml'
-    )
 
-    # Process the Xacro file to generate the URDF representation of the robot
-    robot_description = xacro.process_file(robot_model_path).toxml()
 
-    # Prepare to include the Gazebo simulation launch file
-    gazebo_pkg_launch = PythonLaunchDescriptionSource(
-        os.path.join(
-            get_package_share_directory('ros_gz_sim'),
-            'launch',
-            'gz_sim.launch.py'
-        )
-    )
 
-    # Include the Gazebo launch description with specific arguments
-    gazebo_launch = IncludeLaunchDescription(
-        gazebo_pkg_launch,
-        launch_arguments={
-            'gz_args': [f'-r -v 4 ', world_file],
-            'on_exit_shutdown': 'true'
-        }.items()
-    )
 
-    # Create a node to spawn the robot model in the Gazebo environment
-    spawn_model_gazebo_node = Node(
-        package='ros_gz_sim',
-        executable='create',
-        arguments=[
-            '-name', robot_name,
-            '-string', robot_description,
-            '-x', x,
-            '-y', y,
-            '-z', z,
-            '-R', roll,
-            '-P', pitch,
-            '-Y', yaw,
-            '-allow_renaming', 'false'
-        ],
-        output='screen',
-    )
-
-    # Create a node to publish the robot's state based on its URDF description
-    robot_state_publisher_node = Node(
-        package='robot_state_publisher',
-        executable='robot_state_publisher',
-        parameters=[
-            {'robot_description': robot_description, 'use_sim_time': False}
-        ],
-        output='screen'
-    )
+    robot_descriptionl = Command(['ros2 param get --hide-type /robot_state_publisher robot_description'])
 
     controller_params_file = os.path.join(get_package_share_directory(robot_pkg_name),'config','my_controllers.yaml')
     
     controller_manager = Node(
         package="controller_manager",
         executable="ros2_control_node",
-        parameters=[{'robot_description': robot_description},
+        parameters=[{'robot_description': robot_descriptionl},
                     controller_params_file]
     )
+
+    delayed_controller_manager = TimerAction(period=3.0, actions=[controller_manager])
+
     # diff drive spawner
     diff_drive_spawner = Node(
         package="controller_manager",
         executable="spawner",
         arguments=["diff_cont"],
+    )
+
+    delayed_diff_drive_spawner = RegisterEventHandler(
+        event_handler=OnProcessStart(
+            target_action=controller_manager,
+            on_start=[diff_drive_spawner],
+        )
     )
     # joint broadcast spawner
     joint_broad_spawner = Node(
@@ -131,31 +60,19 @@ def generate_launch_description():
         executable="spawner",
         arguments=["joint_broad"],
     )
-
-    # Create a node for the ROS-Gazebo bridge to handle message passing
-    gz_bridge_node = Node(
-        package='ros_gz_bridge',
-        executable='parameter_bridge',
-        arguments=[
-            '--ros-args', '-p',
-            f'config_file:={gz_bridge_params_path}'
-        ],
-        output='screen'
+    delayed_joint_broad_spawner = RegisterEventHandler(
+        event_handler=OnProcessStart(
+            target_action=controller_manager,
+            on_start=[joint_broad_spawner],
+        )
     )
 
+
+    
+
     return LaunchDescription([
-        world_arg,
-        gazebo_launch,
-        x_arg,
-        y_arg,
-        z_arg,
-        roll_arg,
-        pitch_arg,
-        yaw_arg,
-        spawn_model_gazebo_node,
-        robot_state_publisher_node,
-        gz_bridge_node,
-        diff_drive_spawner,
-        joint_broad_spawner,
-        controller_manager
+        delayed_controller_manager,
+        delayed_diff_drive_spawner,
+        delayed_joint_broad_spawner
+        
     ])
